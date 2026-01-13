e8228a27a33ae431a8c1984905a7dd4b4843e413
mohamed salih va <salihva777@gmail.com>
2026-01-13 17:16:29 +0530

    googleauth routes added and integrate to frontend

--- server/src/routes/AuthRoutes.js
+++ server/src/routes/AuthRoutes.js
@@ -16,7 +16,7 @@ router.post("/login", loginController);
 
 router.get("/google", passport.authenticate("google",{scope: ["profile", "email"]}));
 
-router.get("/google/callback",passport.authenticate("google", { session: false, failureRedirect: "/" }),
+router.get("/google/callback",passport.authenticate("google", { session: false, failureRedirect: "/"}),
   (req, res) => {
     const user = req.user;
 

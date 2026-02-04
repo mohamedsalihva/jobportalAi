de2b30a4e3ff8e32342d0478801199b86d4364b2
mohamed salih va <salihva777@gmail.com>
2026-02-04 10:05:22 +0530

    updated the axios

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,11 +1,14 @@
 
 import axios from "axios";
 
+const resolvedBaseUrl =
+  import.meta.env.VITE_BACKEND_URL || (import.meta.env.PROD ? "/api" : "");
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_BACKEND_URL,
+  baseURL: resolvedBaseUrl,
   withCredentials: true,
 });
-console.log("BACKEND URL:", import.meta.env.VITE_BACKEND_URL);
+console.log("BACKEND URL:", resolvedBaseUrl);
 
 
 export default api;
